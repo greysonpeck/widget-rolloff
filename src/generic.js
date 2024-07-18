fa19f04ef17cc1dc9ffec848845b26380c62f8fb
Greyson Peck <greyson.peck@gmail.com>
2024-07-17 19:01:24 -0700

    edit link again

--- src/generic.js
+++ src/generic.js
@@ -1,5 +1,5 @@
 
 function goTo() {
     d = document.getElementById("links").value;
-    window.location.href = "https://greysonpeck.github.io" + d;
+    window.location.href = "https://greysonpeck.github.io/widget-rolloff" + d;
 }
