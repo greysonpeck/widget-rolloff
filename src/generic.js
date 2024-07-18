f3b70bf3518c799c637356b439f29291e029a387
Greyson Peck <greyson.peck@gmail.com>
2024-07-17 18:55:53 -0700

    Update select link

--- src/generic.js
+++ src/generic.js
@@ -1,6 +1,6 @@
 
 function goTo() {
     d = document.getElementById("links").value;
-    window.location.href = "http://127.0.0.1:5500/" + d;
+    window.location.href = "https://greysonpeck.github.io" + d;
 }
 
